dec279402a68464428b2600b3fb8716e5fbc5bda
Ahmed Harhash <ahmedharhash0048@gmail.com>
2024-12-08 16:19:07 +0200

    configure vite

--- main-project/vite.config.js
+++ main-project/vite.config.js
@@ -1,11 +1,21 @@
-// vite.config.js
 import { defineConfig } from 'vite';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
+import path from 'path';
 
 export default defineConfig({
-  server: {
-    open: true,
-  },
+  plugins: [
+    nodePolyfills({
+      protocolImports: true,
+    }),
+  ],
   build: {
-    outDir: 'dist',  // Output directory for production build
-  },
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+        game: path.resolve(__dirname, 'model_structure.html')
+      }
+    },
+    // Include all relevant assets
+    assetsInclude: ['**/*.{png,jpg,svg,js,json}']
+  }
 });
